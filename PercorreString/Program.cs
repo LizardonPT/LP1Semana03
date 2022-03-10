66f900663c28a4f15b59e2cc8f235ec1b865ec63
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-03-10 12:58:33 +0000

    Percorrestring

---
+++ PercorreString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nEscreva uma palavra ou frase: ");
+            string input = Console.ReadLine();
+
+            Console.Write("\nAgora escreva uma letra usada na palavra ou frase" 
+            + " que escreveu acima: ");
+            char letra = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == letra)
+                {
+                    continue;
+                }
+
+                else
+                {
+                    Console.Write(input[i]);
+                }
+            }
+        }
+    }
+}

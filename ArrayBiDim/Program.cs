3e2aee5dcba51afad892d13fa41ef0188d1db1a5
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-03-18 20:12:00 +0000

    ArrayBiDim

---
+++ ArrayBiDim/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArrayBiDim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nIntroduz um número de linhas: ");
+            int linhas = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nIntroduz um número de colunas: ");
+            int colunas = Convert.ToInt32(Console.ReadLine());
+
+            int[,] tabela = new int[linhas, colunas];
+            
+            for (int i = 0; i < tabela.GetLength(0); i++)
+            {
+                for (int j = 0; j < tabela.GetLength(1); i++)
+                {
+                    Console.Write($"Escreva um numero para a posição {i},{j}: ");
+                    int valor = Convert.ToInt32(Console.ReadLine());
+                    tabela [i, j] = valor;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

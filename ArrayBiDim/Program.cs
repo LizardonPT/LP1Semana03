006c7c384d17bfe437317682ef8712a0951734ea
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-03-19 19:33:36 +0000

    ArrayBiDim terminado

--- ArrayBiDim/Program.cs
+++ ArrayBiDim/Program.cs
@@ -6,6 +6,7 @@ namespace ArrayBiDim
     {
         static void Main(string[] args)
         {
+            //Asks for number os lines and columns
             Console.Write("\nIntroduz um número de linhas: ");
             int linhas = Convert.ToInt32(Console.ReadLine());
 
@@ -14,9 +15,10 @@ namespace ArrayBiDim
 
             int[,] tabela = new int[linhas, colunas];
             
+            //User types the numbers to put in the array
             for (int i = 0; i < tabela.GetLength(0); i++)
             {
-                for (int j = 0; j < tabela.GetLength(1); i++)
+                for (int j = 0; j < tabela.GetLength(1); j++)
                 {
                     Console.Write($"Escreva um numero para a posição {i},{j}: ");
                     int valor = Convert.ToInt32(Console.ReadLine());
@@ -24,6 +26,24 @@ namespace ArrayBiDim
                 }
                 Console.WriteLine();
             }
+
+            
+            float somaMedia = 0f;
+            //Average per line
+            for (int i = 0; i < tabela.GetLength(0); i++)
+            {
+                int somaL = 0;
+                for (int j = 0; j < tabela.GetLength(1); j++)
+                {
+                    somaL += tabela[i, j];
+                }
+                float mediaL = somaL/tabela.GetLength(0);
+                Console.WriteLine($"A média dos valores na linha {i} é = {mediaL}");
+
+                somaMedia += mediaL;
+            }
+
+            Console.WriteLine($"A soma das médias é de {somaMedia}");
         }
     }
 }
